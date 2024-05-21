30936de7acb012c190717bbc88951b739d5540b0
DESKTOP-BHCFH1L <szalachkrzysztof504@gmail.com>
2024-05-21 21:50:14 +0200

    comm

---
+++ src/main/java/com/example/springlogowanie/service/IBookService.java
@@ -0,0 +1,13 @@
+package com.example.springlogowanie.repository;
+
+import com.example.springlogowanie.model.Book;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IBookService {
+    void saveOrUpdate(Book book);
+    Optional<Book> getById(int id);
+    List<Book> getAll();
+    void delete(int id);
+}
